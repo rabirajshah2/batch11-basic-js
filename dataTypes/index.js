16ddf16b3d2df0dc8673701c0da5063c4b08d0e7
rabirajshah <rabirajshah2@gmail.com>
2024-12-22 17:49:46 +0545

    Learned Primitive Data Types

---
+++ dataTypes/index.js
@@ -0,0 +1,48 @@
+// 8 types of Data Types
+
+// 1.String 
+const name="Rabi Raj";
+console.log(name);
+
+// 2.Number
+const num=100;
+console.log(num);
+
+// 3.Bigint
+const int=BigInt("5768146465116154151");
+console.log(int);
+
+
+// 4.Boolean
+const isMale=true;
+const isFemale=false;
+console.log(isMale, isFemale);
+
+// 5.Undefined
+let x;
+console.log(x);
+
+// 6.Null
+const y=null;
+console.log(y);
+
+// 7.Symbol
+const n=Symbol("Rabi");
+console.log(n);
+
+// 8.Object
+const Rabi={name:"Rabi", age:23, dob:""};
+console.log(Rabi);
+
+//Type Conversion
+
+// 1. String to Number
+
+const str="8";
+const num1=Number(str); // Type Conversion
+console.log(12 + num1);
+
+// 2. Number to string
+const num2=20;
+const str1=String(num2); // Type conversion
+console.log("8"+str1);
