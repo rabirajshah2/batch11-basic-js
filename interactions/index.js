a19822f62034b571916acf0d2a73588f55ad7312
rabirajshah <rabirajshah2@gmail.com>
2024-12-22 15:29:03 +0545

    Learned Interactions types

---
+++ interactions/index.js
@@ -0,0 +1,18 @@
+console.log("interactions");
+
+// // 3 types of interactions
+//     1.alert
+//     2.confirm
+//     3.prompt
+
+alert("Hello World");
+
+const user = confirm("Are you male");
+console.log(user);
+
+let name = prompt("What is your name?");
+console.log(name);
+
+const schoolname = prompt("Please tell me your school name");
+console.log(schoolname);
+alert(schoolname);
