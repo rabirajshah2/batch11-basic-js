f224518920b7f82d6a9beb222145eefc3897adca
rabirajshah <rabirajshah2@gmail.com>
2024-12-22 14:38:21 +0545

    Learned variables

---
+++ variables/index.js
@@ -0,0 +1,27 @@
+// REPL (Read, Evalaute, Print and Loop)
+
+// Print
+console.log("Hello World");
+
+// Variables :- Named storage for data
+
+//ES5
+// var is the keyword to call the variables
+
+var person = "Rabi";
+var bat = "red bat";
+person = "mahesh"; //It will automatically override which creates problem later
+console.log(person, bat);
+
+//ES6
+// let, const
+
+const apple = "red apple";
+// apple="blue apple"; // const prevents overriding
+console.log(apple);
+
+
+let ball = "red ball";
+console.log(ball);
+// ball = "blue ball";
+// ball = "white ball";
